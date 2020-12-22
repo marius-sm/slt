b3e0f1de571c05bd0c24d18ccd55e4052a25d257
Marius Schmidt-Mengin <marius.schmidt-mengin@eleves.enpc.fr>
2020-12-22 12:55:30 +0100

    renamed files

--- data/extract_annotations.py
+++ data/extract_annotations.py
@@ -8,7 +8,7 @@ import gzip
 # a list [{'name': ..., 'text': ..., 'gloss': ..., 'signer': ...}]
 # where the 'sign' attribute (the embedding) has been removed.
 
-file = 'data/phoenix14t.pami0.dev.dontsync'
+file = 'data/phoenix14t.pami0.dev'
 
 def load_dataset_file(filename):
     with gzip.open(filename, "rb") as f:
@@ -25,7 +25,7 @@ print('Reading file...')
 for s in tmp:
     samples.append({k: v for k, v in s.items() if k != 'sign'})
 
-with open(f'{file}.no_embeddings', 'wb') as output:
+with open(f'{file}.annotations_only', 'wb') as output:
     pickle.dump(samples, output, pickle.HIGHEST_PROTOCOL)
 
 print('Successfully wrote new file')
